2b1c57e0a11cb9896e157f08dc631ae804fa4313
Kamil <kamil.klosek2@gmail.com>
2019-10-25 19:42:10 +0200

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -88,7 +88,7 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
-
+{{--test--}}
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
