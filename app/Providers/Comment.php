60429ad01773db1bcf66ab2b22c0db37e800b47a
BlackBurn <BlackBurn@DESKTOP-3SB31V4>
2019-11-14 20:58:02 +0100

    avatar/comment model

---
+++ app/Providers/Comment.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    protected $fillable = [
+        'id', 'post_id','user_id','content', 'rating'
+    ];
+
+
+    public function user(){
+
+        return $this->belongsTo('App\User');
+
+    }
+}
