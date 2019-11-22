9236f5edbf2eb6f6708f9bf5f67f07e15e354e69
Kamil <kamil.klosek2@gmail.com>
2019-11-22 14:03:04 +0100

    test

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -11,7 +11,7 @@ class Kernel extends HttpKernel
      *
      * These middleware are run during every request to your application.
      *
-     * @var array ssdasd
+     * @var array ssdasd5234523
      */
     protected $middleware = [
         \App\Http\Middleware\CheckForMaintenanceMode::class,
