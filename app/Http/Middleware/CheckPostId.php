4b1e1e068cd5dc3708cdfd86a8dc25bade56fd67
Kamil <kamil.klosek2@gmail.com>
2019-12-09 14:21:27 +0100

    walidacja postów

---
+++ app/Http/Middleware/CheckPostId.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Post;
+use Closure;
+
+class CheckPostId
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        $post = Post::latest()->first();
+
+        $id = $request->route('id');
+
+
+        if(filter_var($id, FILTER_VALIDATE_INT) === false || $post->id < $id){
+            return redirect('/');
+        }
+
+
+
+        return $next($request);
+    }
+}
